1562b8a344015b825610b223c8ede97ed1693d7c
pmackle <pimackle@ucdavis.edu>
2021-01-17 10:54:57 +0000

    Proper comment structure, from Patrick

--- sshell.c
+++ sshell.c
@@ -2,21 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-// fork(), exec(), wait()
 
-// given assumptions of limits
+// Provided assumptions.
 #define MAX_CMDLINE_SIZE 512
 #define MAX_NUM_ARGS 16
 #define MAX_TOKEN_SIZE 32
 
-// for builtin functions show that it worked
+// Display success of builtin functions.
 // void display_success(char* cmd) 
 // {
 // 	fprintf(stderr, "+ completed '%s' ", cmd);
 // 	fprintf(stderr, "[%i]\n", EXIT_SUCCESS);
 // }
 
-// resolve user input into thre unique elements
+// Resolve user input into thre unique elements.
 struct CommandLine {
 	char command[MAX_CMDLINE_SIZE];
 	int num_args;
@@ -24,21 +23,21 @@ struct CommandLine {
 };
 
 void parse_user_input(struct CommandLine *MyCommandLine, char* user_input) {
-	// copy to preserve original input
+	// Copy to preserve original input.
 	char user_input_copy[MAX_CMDLINE_SIZE];
     strcpy(user_input_copy, user_input);
-    //https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
+    // Derived from https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm.
     
 	const char delim[2] = " ";
     char* token;
 
-    /* get the first token */
+    // Get the first token describing the command.
     token = strtok(user_input_copy, delim);
 	strcpy(MyCommandLine->command, token);
-    /* walk through other tokens */
+    // Fill args with remaining tokens.
     int f = 0;
 	token = strtok(NULL, delim);
-    while( token != NULL ) {
+    while (token != NULL) {
         // command->args = (char **) realloc(command->args, (command->num_args)*sizeof(char*));
 		strcpy(MyCommandLine->args[f], token);
 		MyCommandLine->num_args = MyCommandLine->num_args + 1;
@@ -60,21 +59,21 @@ int main(void)
 	 	char *nl;
 		//int retval;
 
-		/* Print prompt */
+		// Print prompt.
 		printf("sshell$ ");
 		fflush(stdout);
 
-		/* Get command line */
+		// Get command line.
 		fgets(user_input, MAX_CMDLINE_SIZE, stdin);
 
 		// command.cmd = cmd;
-		/* Print command line if stdin is not provided by terminal */
+		// Print command line if stdin is not provided by terminal.
 		if (!isatty(STDIN_FILENO)) {
 			printf("%s", user_input);
 			fflush(stdout);
 		}
 
-		/* Remove trailing newline from command line */
+		// Remove trailing newline from command line.
 		nl = strchr(user_input, '\n');
 		if (nl)
 			*nl = '\0';
@@ -82,24 +81,24 @@ int main(void)
         parse_user_input(&MyCommandLine, user_input);
 		// printf("%s\n%d\n", MyCommandLine.command, MyCommandLine.num_args);
 		
-	// 	/* Builtin command */
+	// 	// Builtin command.
 	// 	if (!strcmp(cmd, "exit")) {
 	// 		fprintf(stderr, "Bye...\n");
 	// 		display_success(cmd);
 	// 		break;
 	// 	} else if (cmd[0] == 'c' && cmd[1] == 'd') {
 			
-	// 		/* strkok modifies original string, so we keep copy for printing */
+	// 		// The strkok function modifies original string, so we keep a copy for printing.
 	// 		char cmd_copy[MAX_CMDLINE_SIZE];
 	// 		strcpy(cmd_copy, cmd);
-	// 		//https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
+	// 		// Derived from https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm.
 	// 		const char delim[2] = " ";
 	// 		char* token;
 			
-	// 		/* get the first token */
+	// 		// Get the first token.
 	// 		token = strtok(cmd_copy, delim);
 			
-	// 		/* walk through other tokens */
+	// 		// Walk through other tokens.
 	// 		token = strtok(NULL, delim);
 	// 		//printf("%s\n", token);
 	// 		chdir(token);
@@ -111,10 +110,11 @@ int main(void)
 	// 		fprintf(stdout, "\n");
 	// 		display_success(cmd);
 	// 	} else {
-	// 		/* Regular command */
-	// 		//retval = system(cmd); // needs to be replaced w/ fork+exec+wait
-	// 		//fprintf(stdout, "Return status value for '%s': %d\n", cmd, retval);
+	// 		// Regular command.
+	// 		// retval = system(cmd); // needs to be replaced w/ fork+exec+wait
+	// 		// fprintf(stdout, "Return status value for '%s': %d\n", cmd, retval);
 	// 	}
 	}
+    
 	return EXIT_SUCCESS;
 }
