2c9bf2741ef702b51cff2bbf59624a0c2e21bec4
Patrick Mackle <pimackle@ucdavis.edu>
2021-01-18 09:38:06 +0000

    Added output redirection

--- sshell.c
+++ sshell.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 // Provided assumptions.
 #define MAX_CMDLINE_SIZE 512
@@ -14,6 +15,7 @@
 struct CommandLine {
 	int argc;
 	char* argv[MAX_NUM_ARGS];
+	char* redirect_file;
 };
 
 // Display success of builtin functions.
@@ -27,21 +29,35 @@ void parse_command_line(struct CommandLine* MyCommandLine, char* command_line)
 {
 	// Copy to preserve original input.
 	char command_line_copy[MAX_CMDLINE_SIZE];
-    strcpy(command_line_copy, command_line);
+	strcpy(command_line_copy, command_line);
+
+	const char arg_delim[2] = " ";
+
+	char* positionptr = strchr(command_line, '>');
+	if (positionptr) {
+		char command_line_copy2[MAX_CMDLINE_SIZE];
+		strcpy(command_line_copy2, command_line);
+
+		const char redirect_delim[2] = ">";
+
+		char* current_token2 = strtok(command_line_copy2, redirect_delim);
+		strcpy(command_line_copy, current_token2);
+
+		current_token2 = strtok(NULL, redirect_delim);
+		MyCommandLine->redirect_file = strtok(current_token2, arg_delim);
+	}
 
     // Derived from https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm.
-	const char delim[2] = " ";
     char* current_token;
 
     // Get the first token describing the command.
-    current_token = strtok(command_line_copy, delim);
+    current_token = strtok(command_line_copy, arg_delim);
     // Fill args with remaining tokens.
     int i = 0;
     while (current_token != NULL) {
-
 		MyCommandLine->argv[i] = current_token;
 		MyCommandLine->argc = MyCommandLine->argc + 1;
-		current_token = strtok(NULL, delim);
+		current_token = strtok(NULL, arg_delim);
 
         i++;
    }
@@ -56,7 +72,6 @@ int has_errors(struct CommandLine* MyCommandLine)
         return EXIT_FAILURE;
     }
 	
-	//if ()
 	return EXIT_SUCCESS;
 }
 
@@ -88,9 +103,17 @@ int main(void)
 			*nl = '\0';
 
         parse_command_line(&MyCommandLine, command_line);
-		if (has_errors(&MyCommandLine)) {
-            return EXIT_FAILURE;
+		if (has_errors(&MyCommandLine) == EXIT_FAILURE) {
+            continue;
         }
+
+		//printf("%s\n", MyCommandLine.redirect_file);
+		if(MyCommandLine.redirect_file){
+			int fd;
+			fd = open(MyCommandLine.redirect_file, O_WRONLY | O_CREAT, 0644);
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
 
         if (!strcmp(MyCommandLine.argv[0], "exit")) {
 		    fprintf(stderr, "Bye...\n");
@@ -127,7 +150,16 @@ int main(void)
                 return EXIT_FAILURE;
             }
         }
+
+		//dup2(STDOUT_FILENO, fd);
+		
 	}
 
 	return EXIT_SUCCESS;
 }
+
+/*
+else if (!strcmp(MyCommandLine.argv[0], "set")) {
+	MyCommandLin
+}
+*/
